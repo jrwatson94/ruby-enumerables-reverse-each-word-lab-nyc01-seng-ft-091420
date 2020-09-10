a060d235610edaef38292b47b7868122674aefc7
Reid Watson <j.watsonreid@gmail.com>
2020-09-10 22:46:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(sentence)
   #takes in a string argument and 
   #returns same sentence with each word reversed in place
   sentence_array = sentence.split(/ /)
-  
-  reversed_word = ""
+
   sentence_array.each do |word|
+    
   
   end
 
